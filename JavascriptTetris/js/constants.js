b1b9cfdccc5f9d554b533a83f5935ffb815511d8
Wilkinson, Kevin Mitchell - (willkillson) <willkillson@gmail.com>
2020-01-25 04:04:28 -0700

    added some kind of organization

---
+++ JavascriptTetris/js/constants.js
@@ -0,0 +1,14 @@
+
+const GRIDHEIGHT = 25;//og 25
+const GRIDWIDTH = 12;//og 12
+const CELLSIZE = 30;//og 30
+const CELLPADDING = 1;//og 1
+
+const red = "rgba(255,51,51,0.9)";
+const teal = "rgba(51,255,255,0.9)";
+const orange = "rgba(255,153,51,0.9)";
+const blue = "rgba(51,51,255,0.9)";
+const purple = "rgba(153,51,255,0.9)";
+const green = "rgba(51,255,51,0.9)";
+const yellow = "rgba(255,255,51,0.9)";
+const black = "rgba(0,0,0,0.9)";
